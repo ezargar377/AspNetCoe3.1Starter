85278c524a9255ba1794ba426bc69df2a1ba0e44
ehsan <zargar.ehsan@gmail.com>
2020-05-10 13:35:40 +0430

    initial commit

---
+++ Web.Api/Controllers/ProtectedController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Api.Controllers
+{
+    [Authorize /*(Policy = "ApiUser")*/]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProtectedController : ControllerBase
+    {
+        // GET api/protected/home
+        [HttpGet]
+        public IActionResult Home()
+        {
+            return new OkObjectResult(new { result = true });
+        }
+        
+        [HttpGet("[action]")]
+
+        [Authorize (Policy = "ApiUser")]
+        public IActionResult ApiUserPolicy()
+        {
+            return new OkObjectResult(new { result = true });
+        }
+    }
+}
